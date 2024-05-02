e33e1a5f013b5a1c1f29f614f88ebeae99fca71f
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2024-05-02 18:43:47 +0200

    Initial Commit

---
+++ SatelliTrack/SatelliTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatelliTrackApp.swift
+//  SatelliTrack
+//
+//  Created by Hugo Pereira on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SatelliTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
