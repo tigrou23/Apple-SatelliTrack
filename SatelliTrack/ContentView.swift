7368b367cb0aa207ff17575e787df1789928fe76
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2024-05-03 10:13:59 +0200

    add reset button

--- SatelliTrack/ContentView.swift
+++ SatelliTrack/ContentView.swift
@@ -11,24 +11,47 @@ import MapKit
 struct ContentView: View {
     
     @State private var satellites: [Satellite] = []
-    let FILE_NAME = "satellite_data.txt"
+    @State private var showingAlert = false
+    let FILE_NAME = "satellite_tle.txt"
 
     var body: some View {
         VStack {
+
             MapView(satellites: satellites)
                 .edgesIgnoringSafeArea(.all)
                 .onAppear {
-                    DownloadManager.shared.downloadFile()
-                    // Get the file URL
-                    let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-                    let fileURL = documentsURL.appendingPathComponent(FILE_NAME)
-                                
-                    // Parse the TLE file to extract the satellite information
-                    if let satellites = TLEParser.parseTLEFile(at: fileURL) {
-                        self.satellites = satellites
-                    }
+                    //DownloadManager.shared.downloadFile()
+                    updateFromFile()
                 }
+            .alert("bad data, maybe too many queries?", isPresented: $showingAlert) {
+                Button("OK", role: .cancel) { }
+            }
+            let message: String = String(satellites.count);
+            Text("Number of satellites: \(message)")
+            Button("Reset View"){
+                updateFromFile()
+            }
+            Button("Download TLE File"){
+                DownloadManager.shared.downloadFile()
+                sleep(4)
+                updateFromFile()
+            }
+            
         }
+    }
+    
+    func updateFromFile(){
+        // Get the file URL
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsURL.appendingPathComponent(FILE_NAME)
+                    
+        // Parse the TLE file to extract the satellite information
+        if let satellites = TLEParser.parseTLEFile(at: fileURL) {
+            self.satellites = satellites
+        }
+        
+        if satellites.count == 0 { showingAlert = true }
+        
     }
 }
 
