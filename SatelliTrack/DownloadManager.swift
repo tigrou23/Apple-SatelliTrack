d560663181eebfe5def01d9ddd1e6166f3a22e87
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2024-05-03 10:13:43 +0200

    download will erase old data

--- SatelliTrack/DownloadManager.swift
+++ SatelliTrack/DownloadManager.swift
@@ -12,10 +12,14 @@ class DownloadManager {
     static let shared = DownloadManager()
     
     let CELESTRAK_URL = URL(string: "https://celestrak.org/NORAD/elements/gp.php?GROUP=globalstar&FORMAT=tle")!
-    let FILE_NAME = "satellite_data.txt"
+    let FILE_NAME = "satellite_tle.txt"
     
     /// Download the TLE file from Celestrak
     func downloadFile() {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let destinationURL = documentsURL.appendingPathComponent(self.FILE_NAME)
+        try? FileManager.default.removeItem(at: destinationURL)
+
         URLSession.shared.downloadTask(with: CELESTRAK_URL) { (location, response, error) in
             guard let location = location, error == nil else {
                 print("Erreur de téléchargement : \(error!.localizedDescription)"); return
@@ -24,6 +28,7 @@ class DownloadManager {
             let destinationURL = documentsURL.appendingPathComponent(self.FILE_NAME)
             try? FileManager.default.moveItem(at: location, to: destinationURL)
             print("Téléchargement terminé. Fichier enregistré à : \(destinationURL)")
+            
         }.resume()
     }
 }
